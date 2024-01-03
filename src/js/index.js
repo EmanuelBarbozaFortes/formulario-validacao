91ca295a07aede2ce8b59b01af5d4d8f574bc99a
Emanuel Fortes <emanuelbf19@gmail.com>
2024-01-03 20:39:31 -0300

    Projeto Finalizado
    Projeto Finalizado

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+let inputText = document.querySelectorAll(".input");
+const formulario = document.querySelector('#meuForm')
+
+formulario.addEventListener('submit',(event) =>{
+    event.preventDefault();
+    inputText.forEach(item =>{
+        if (item.value === "") {
+            item.classList.add('invalid');
+            item.nextElementSibling.classList.add('invalid')
+        } else {
+            alert("Sua mensagem foi enviada, lembrando aperte OK 4 vezes")
+        }
+    })
+})
+
+inputText.forEach(input => {
+    input.addEventListener('change', () => {
+        if (input.value !== "") {
+            input.classList.add('valid');
+            input.classList.remove('invalid');
+            input.nextElementSibling.classList.remove('invalid')
+        } else {
+            input.classList.remove('valid');
+        }
+    });
+});
